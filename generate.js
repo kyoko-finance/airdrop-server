d667f65144011ad3478326a1f74967bee9d26bf6
mattfixme <matt@kyoko.finance>
2022-04-07 14:19:48 +0800

    feat: internal test

--- generate.js
+++ generate.js
@@ -1,65 +1,14 @@
 const { ethers } = require('ethers');
 const keccak256 = require('keccak256');
 const { MerkleTree } = require('merkletreejs');
-const { format } = require('mathjs');
 
-const dataList = [
-    {
-        address: '0xA998b62aD18B5a5FcFB615Dd27B8EA9Fa0ac0AA9',
-        amount: format(2000 * 1e18, { notation: 'fixed' }),
-        index: 1
-    },
-    {
-        address: '0x22Fad004b0cC5DE0B45df49FDFD4aa7b438a5d27',
-        amount: format(200 * 1e18, { notation: 'fixed' }),
-        index: 1
-    },
-    {
-        address: '0x3D3B6B89b87e1c02C1740850Bda4FB3eE7846279',
-        amount: format(200 * 1e18, { notation: 'fixed' }),
-        index: 1
-    },
-    {
-        address: '0xA998b62aD18B5a5FcFB615Dd27B8EA9Fa0ac0AA9',
-        amount: format(2000 * 1e18, { notation: 'fixed' }),
-        index: 2
-    },
-    {
-        address: '0x22Fad004b0cC5DE0B45df49FDFD4aa7b438a5d27',
-        amount: format(200 * 1e18, { notation: 'fixed' }),
-        index: 2
-    },
-    {
-        address: '0x3D3B6B89b87e1c02C1740850Bda4FB3eE7846279',
-        amount: format(200 * 1e18, { notation: 'fixed' }),
-        index: 2
-    },
-    {
-        address: '0xA998b62aD18B5a5FcFB615Dd27B8EA9Fa0ac0AA9',
-        amount: format(2000 * 1e18, { notation: 'fixed' }),
-        index: 3
-    },
-    {
-        address: '0x22Fad004b0cC5DE0B45df49FDFD4aa7b438a5d27',
-        amount: format(200 * 1e18, { notation: 'fixed' }),
-        index: 3
-    },
-    {
-        address: '0x3D3B6B89b87e1c02C1740850Bda4FB3eE7846279',
-        amount: format(200 * 1e18, { notation: 'fixed' }),
-        index: 3
-    }
-];
+const dataList = require('./data1.json');
 
 const leafNodes = dataList.map(node => {
     return ethers.utils.solidityKeccak256(['address', 'uint256', 'uint8'], [node.address, node.amount, node.index])
 });
 
 const merkletree = new MerkleTree(leafNodes, keccak256, { sortPairs: true });
-
-const root = merkletree.getHexRoot();
-
-console.log("root: ", root);
 
 module.exports = (address, index) => {
     const node = dataList.find(node => {
